57e3eb7f3a18dd85944392ca6695c6669734ebae
Sarbjit Singh <111473367+SarbjitSingh001@users.noreply.github.com>
2023-03-17 21:52:25 +0000

    Fri, Mar 17, 2023, 2:52 PM -07:00

--- js/sounds.js
+++ js/sounds.js
@@ -2,4 +2,10 @@ const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 
 sounds.forEach(sound => {
     const btn = document.createElement('button')
+    btn.classList.add('btn')
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        document.getElementById(sound).play()
+    })
 })
