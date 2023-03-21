3ebb871915882b3c118116c94f9e45820efcddca
Sarbjit Singh <111473367+SarbjitSingh001@users.noreply.github.com>
2023-03-21 21:50:30 +0000

    Tue, Mar 21, 2023, 2:50 PM -07:00

--- js/counter.js
+++ js/counter.js
@@ -4,9 +4,17 @@ counters.forEach(counter => {
     counter.innerText = '0'
 
     const updateCounter = () => {
-        const target = counter.getAttribute('data-target')
-        console.log(target)
+        const target = +counter.getAttribute('data-target')
+        const c = +counter.innerText
+        const increment = 1
+
+        if (c < target) {
+            counter.innerText = `${Math.ceiling}`
+        }
+
+
     }
 
     updateCounter()
 })
+
