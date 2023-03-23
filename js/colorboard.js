bb9c282f8867cdf121cfa7dabc59308c9af35424
Sarbjit Singh <111473367+SarbjitSingh001@users.noreply.github.com>
2023-03-23 21:08:11 +0000

    Thu, Mar 23, 2023, 2:08 PM -07:00

--- js/colorboard.js
+++ js/colorboard.js
@@ -6,5 +6,10 @@ for (let i = 0; i < SQUARES; i++) {
     const square = document.createElement('div')
     square.classList.add('square')
     container.appendChild(square)
-    
+    square.addEventListener('mouseover', () => setColor(square))
+}
+
+function setColor(element) {
+
+    element.style.background = 'yellow'
 }
