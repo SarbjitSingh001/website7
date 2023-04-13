7aa727882bfddf350980fb5879050e353f32eedd
Sarbjit Singh <111473367+SarbjitSingh001@users.noreply.github.com>
2023-04-13 21:54:44 +0000

    Thu, Apr 13, 2023, 2:54 PM -07:00

--- js/website.js
+++ js/website.js
@@ -5,3 +5,19 @@ const search = document.querySelector('.search')
 button.addEventListener('click', () => {
     search.classList.toggle('active')
 })
+
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActive()
+        panel.classList.add('active')
+    })
+})
+
+
+function removeActive() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
